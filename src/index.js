0ebb6ae32ed7c0d30bdc81b51ef1bde1f4f8e1ac
Jaid <jaid.jsx@gmail.com>
2019-11-30 18:11:08 +0100

    doc: Removed wrong description
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@
 import simpleGit from "simple-git/promise"
 
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
  * @function
- * @async
  * @param {string} directory Absolute path to a git repository directory
- * @returns {boolean|null} `true` if repository is dirty, `false` if repository is clean, `null` if given directory is not a git repository
+ * @returns {Promise<boolean|null>} `true` if repository is dirty, `false` if repository is clean, `null` if given directory is not a git repository
  * @example
  * import isGitRepoDirty from "is-git-repo-dirty"
  * const result = await isGitRepoDirty("/my/path")
